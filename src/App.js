8bae813df6b64467c4db942861a9314199321cf7
nathan-08 <nathanryan001@gmail.com>
2018-08-14 14:11:53 -0600

    readme

--- src/App.js
+++ src/App.js
@@ -1,85 +1,115 @@
 import React, { Component } from 'react';
 import './App.css';
-const testImg = 'http://media.comicbook.com/2018/07/squirtle-1-1119670-1280x0.jpeg'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products:[
+        {
+          id:1,
+          name:'Flip Flops',
+          description:'Some flippy floppys',
+          price:5.99,
+          imageUrl:'http://via.placeholder.com/350x150'
+        },
+        {
+          id:2,
+          name:'Tent',
+          description:'TENTS',
+          price:6.99,
+          imageUrl:'http://via.placeholder.com/350x150'
+        },
+        {
+          id:3,
+          name:'Sun tan lotion',
+          description:'Gotta look fly guy',
+          price:7.99,
+          imageUrl:'http://via.placeholder.com/350x150'
+        },
+        {
+          id:4,
+          name:'Mice',
+          description:'Not blind',
+          price:8.99,
+          imageUrl:'http://via.placeholder.com/350x150'
+        },
+      ],
+      cart:[]
+    }
+  }
+  handleAddItemToCart( item ){
+    let newCart = this.state.cart.map( cartItem => {
+      return {
+        id:cartItem.id,
+        name:cartItem.name,
+        description:cartItem.description,
+        price:cartItem.price,
+        imageUrl:cartItem.imageUrl
+      }
+    })
+    newCart.push(item)
+    this.setState({
+      cart:newCart
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <section className="left_side">
-        <div className="center_title"><h1>Products</h1></div>
-
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description</p>
-          </div>
+      <div>
+        <div className='products'>
+          <h1>PRODUCTS</h1>
+          {
+            this.state.products.map( item => {
+              return(
+                <div>
+                  <img src={item.imageUrl}/>
+                  <h4>{item.name}</h4>
+                  <p>{item.description}</p>
+                  <p>{item.price}</p>
+                  <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                </div>
+              )
+            })
+          }
+          
         </div>
-
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
+        <div className='cart'>
+          <h1>CART</h1>
+          {
+            this.state.cart.map( item => {
+              return( 
+                <div>
+                  <img src={item.imageUrl}/>
+                  <h4>{item.name}</h4>
+                  <p>{item.description}</p>
+                  <p>{item.price}</p>
+                  <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                </div>
+              )
+            })
+          }
         </div>
-
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
-        </div>
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
-        </div>
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
-        </div>
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
-        </div>
-        <div className="product_container clearfix">
-          <img src={testImg} alt="product_image"/>
-          <div className="product_description">
-              <h3>Test_Product</h3>
-              <p>test description</p>
-          </div>
-        </div>
-        
-
-        </section>
-
-
-
-
-
-
-        <section className="right_side">
-        <div className="center_title"><h1>Cart</h1></div>
-        <ul>
-          <li>list_item</li>
-          <li>list_item</li>
-          <li>list_item</li>
-        </ul>
-        </section> 
       </div>
     );
   }
 }
 
 export default App;
+
+
+// 1 - single screen, sidebar, left side is a list of products.
+// 2 - add in props - list component, list item component, clean it up
+// 3 - add in axios.
+// Each day every project has 4 stages
+// day 1
+// - 1 mini project + 20-30% more
+//     - class, functional, state
+//     - list of products, pre-hardcoded onto state. Click add to cart and it adds it to a list on the sidebar. Clicking add to cart multiple times increases the qty.
+// - 2 is where the afternoon project is now
+//     - get a total, left side, multiple categories. Header for each category that is not seperate component, different background color or header color for each category. Checkout btn, cart is cleared. No nesting state.Each category is a seperate property on state
+// - 3 including concepts from previous days: never super complex
+//     - user input validation
+//     - fake credit card payment
+// - 4 black diamond - almost no direction
+//     - Delete item out of cart, update quantity, update between list and card view on product side. Idea is conditional rendering.
+// Stage 3-4 need to stay independant of other projects. These are the push yourselves levels.
