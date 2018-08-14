6ec59ef52c6d9ddedaf415eeb03b048a7a2f6479
nathan-08 <nathanryan001@gmail.com>
2018-08-14 11:38:15 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const testImg = 'http://media.comicbook.com/2018/07/squirtle-1-1119670-1280x0.jpeg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="left_side">
+        <div className="center_title"><h1>Products</h1></div>
+
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description</p>
+          </div>
+        </div>
+
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+        <div className="product_container clearfix">
+          <img src={testImg} alt="product_image"/>
+          <div className="product_description">
+              <h3>Test_Product</h3>
+              <p>test description</p>
+          </div>
+        </div>
+        
+
+        </section>
+
+
+
+
+
+
+        <section className="right_side">
+        <div className="center_title"><h1>Cart</h1></div>
+        <ul>
+          <li>list_item</li>
+          <li>list_item</li>
+          <li>list_item</li>
+        </ul>
+        </section> 
       </div>
     );
   }
