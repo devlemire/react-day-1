2be4a581691b3b89345357f7c6a661d8f7e5f7f4
nathan-08 <nathanryan001@gmail.com>
2018-08-22 11:39:55 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -1,168 +1,179 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      beachGear:[
+      addressInput: "",
+      ccInput: "",
+      beachGear: [
         {
-          id:1,
-          name:'Flip Flops',
-          description:'Some flippy floppys',
-          price:5.99,
-          imageUrl:'http://via.placeholder.com/350x150',
-          quantity:0
+          id: 1,
+          name: "Flip Flops",
+          description: "Some flippy floppys",
+          price: 5.99,
+          imageUrl:
+            "https://i.pinimg.com/736x/86/51/8c/86518c2adfb760bf5e9091841ab6fc9c--girls-flip-flops-beach-flip-flops.jpg"
         },
         {
-          id:2,
-          name:'Tent',
-          description:'TENTS',
-          price:6.99,
-          imageUrl:'http://via.placeholder.com/350x150',
-          quantity:0
-        },
+          id: 3,
+          name: "Sun tan lotion",
+          description: "Gotta look fly guy",
+          price: 7.99,
+          imageUrl:
+            "https://images.all-free-download.com/images/graphicthumb/summer_cream_protect_lotion_design_vector_582414.jpg"
+        }
       ],
-        camping:[
-          {
-            id:3,
-            name:'Sun tan lotion',
-            description:'Gotta look fly guy',
-            price:7.99,
-            imageUrl:'http://via.placeholder.com/350x150',
-            quantity:0
-          },
-          {
-            id:4,
-            name:'Mice',
-            description:'Not blind',
-            price:8.99,
-            imageUrl:'http://via.placeholder.com/350x150',
-            quantity:0
-          },
+      camping: [
+        {
+          id: 2,
+          name: "Tent",
+          description: "TENTS",
+          price: 6.99,
+          imageUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhEXFLblyNPD1fknwWEaJ-sc_t6o0NANN1ZHltRn6CuEbJUTT0aQ"
+        },
+        {
+          id: 4,
+          name: "Mice",
+          description: "Not blind",
+          price: 8.99,
+          imageUrl:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Assorted_computer_mice_-_MfK_Bern.jpg/220px-Assorted_computer_mice_-_MfK_Bern.jpg"
+        }
+      ],
 
-        ],
-      
-      cart:[],
-      address:'',
-      creditCard:''
-    }
+      cart: []
+    };
     this.checkout = this.checkout.bind(this);
   }
-  handleAddItemToCart( item ){
-    let newCart = this.state.cart.map( cartItem => {
+  handleAddressInput = event => this.setState({ addressInput: event.target.value });
+  handleCCInput = event => this.setState({ ccInput: event.target.value });
+  handleAddItemToCart(item) {
+    let newCart = this.state.cart.map(cartItem => {
       return {
-        id:cartItem.id,
-        name:cartItem.name,
-        description:cartItem.description,
-        price:cartItem.price,
-        imageUrl:cartItem.imageUrl,
-        quantity:cartItem.quantity
-      }
-    })
-    let itemIndex = newCart.findIndex( cartItem => cartItem.id === item.id)
-    if( itemIndex!== -1){
-      newCart[itemIndex].quantity++
+        id: cartItem.id,
+        name: cartItem.name,
+        description: cartItem.description,
+        price: cartItem.price,
+        imageUrl: cartItem.imageUrl
+      };
+    });
+    newCart.push(item);
+    this.setState({
+      cart: newCart
+    });
+  }
+  checkout() {
+    if (!this.state.addressInput || !this.state.ccInput) {
+      alert("Fill out required fields first.")
+    } else if (this.state.cart.length === 0) {
+      alert("cart is empty.")
     } else {
-      item.quantity++
-      newCart.push(item)
-    }
-    this.setState({
-      cart:newCart
-    })
-  }
-  
-  checkout(){
-    if(!this.state.address || !this.state.creditCard){
-      alert("WHERE YOU LIVE. WHERE MY MONEY")
-    }
-    else{
-      alert("Here's yer stuff")
+      alert("Here's yer stuff");
       this.setState({
-        cart:[]
-      })
+        cart: [],
+        addressInput: '',
+        ccInput: ""
+      });
     }
-    
   }
-  handleAddressInput( address ){
-    this.setState({
-      address
-    })
-  }
-  handleCreditCardInput( creditCard ){
-    this.setState({
-      creditCard
-    })
-  }
-  
   render() {
     return (
       <div>
-        <div className='products'>
-          <h1>PRODUCTS</h1>
-          <h2>Beach Gear</h2>
-          {
-            this.state.beachGear.map( item => {
-              return(
-                <div>
-                  <img src={item.imageUrl} alt='the item'/>
-                  <h4>{item.name}</h4>
-                  <p>{item.description}</p>
-                  <p>{item.price}</p>
-                  <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
-                </div>
-              )
-            })
-          }
-          <h2>Camping</h2>
-          {
-            this.state.camping.map( item => {
-              return(
-                <div>
-                  <img src={item.imageUrl} alt='the item'/>
-                  <h4>{item.name}</h4>
-                  <p>{item.description}</p>
-                  <p>{item.price}</p>
-                  <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
-                </div>
-              )
-            })
-          }
-          
-        </div>
-        <div className='cart'>
-          <h1>CART</h1>
-          {
-            this.state.cart.map( item => {
-              return( 
-                <div>
-                  <h4>{item.name}</h4>
-                  <p>${item.price}</p>
-                  <p>Quantity: {item.quantity}</p>
-                </div>
-              )
-            })
-          }
-
-        </div>
-        <div className='inputs'>
-          <input placeholder='address' value={this.state.address} onChange={ (e) => this.handleAddressInput(e.target.value)}/>
-          <input placeholder='credit card info' value={this.state.creditCard} onChange={ (e) => this.handleCreditCardInput(e.target.value)}/>
-        </div>
-        <div className='total'>
-          <h1>TOTAL</h1>
-          <p>${
-            this.state.cart.reduce( ( accumulator, current ) => accumulator+= current.price*current.quantity,0)
-          }</p>
-          <button onClick={this.checkout}>Checkout</button>
-        </div>
+        <section className="products">
+          <div className="products_header">
+            <h1>PRODUCTS</h1>
+          </div>
+          <table className="products_body">
+            <tr>
+              <td>
+                <h2>Beach Gear</h2>
+              </td>
+            </tr>
+            {this.state.beachGear.map(item => {
+              return (
+                <tr className="product_container clearfix">
+                  <td>
+                    <img className="product_img" src={item.imageUrl} />
+                  </td>
+                  <td>
+                    <h4>{item.name}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                  </td>
+                </tr>
+              );
+            })}
+            <h2>Camping</h2>
+            {this.state.camping.map(item => {
+              return (
+                <tr className="products_container clearfix">
+                  <td>
+                    <img className="product_img" src={item.imageUrl} />
+                  </td>
+                  <td>
+                    <h4>{item.name}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </section>
+        <section className="cart">
+          <div className="cart_header">
+            <h1>CART</h1>
+            <div className="total">
+              <table>
+                <tr>
+                  <td>
+                    <label>address</label>
+                  </td>
+                  <td>
+                    <input type="text" value={this.state.addressInput} onChange={this.handleAddressInput} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label>credit card number</label>
+                  </td>
+                  <td>
+                    <input type="text" value={this.state.ccInput} onChange={this.handleCCInput} />
+                  </td>
+                </tr>
+              </table>
+              <h4>TOTAL</h4>
+              <p>${this.state.cart.reduce((accumulator, current) => (accumulator += current.price), 0).toFixed(2)}</p>
+              <button onClick={this.checkout}>Checkout</button>
+            </div>
+          </div>
+          <table className="cart_body">
+            {this.state.cart.map(item => {
+              return (
+                <tr className="products_container clearfix">
+                  <td>
+                    <img className="product_img" src={item.imageUrl} />
+                  </td>
+                  <td>
+                    <h4>{item.name}</h4>
+                    <p>{item.price}</p>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </section>
       </div>
     );
   }
 }
 
 export default App;
-
 
 // 1 - single screen, sidebar, left side is a list of products.
 // 2 - add in props - list component, list item component, clean it up
@@ -177,7 +188,6 @@ export default App;
 // - 3 including concepts from previous days: never super complex
 //     - user input validation
 //     - fake credit card payment
-//     - item displays quantity
 // - 4 black diamond - almost no direction
 //     - Delete item out of cart, update quantity, update between list and card view on product side. Idea is conditional rendering.
 // Stage 3-4 need to stay independant of other projects. These are the push yourselves levels.
